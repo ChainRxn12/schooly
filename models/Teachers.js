8b3a14f9528b1f6cdb28e0216c7bffe54e483bd1
jafet <jafet.manauth@Hotmail.com>
2021-08-03 19:13:33 -0500

    made models files

---
+++ models/Teachers.js
@@ -0,0 +1,69 @@
+const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
+
+class Teacher extends Model {}
+
+Teacher.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+            },
+        },
+        password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            len: [1],
+            },
+        },
+    },
+    {
+        hooks: {
+        beforeCreate: async (newReader) => {
+        try {
+            newReader.password = await bcrypt.hash(newReader.password, 10);
+            return newReader;
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+        },
+        beforeUpdate: async (updatedReader) => {
+        try {
+            updatedReader.password = await bcrypt.hash(
+            updatedReader.password,
+            10
+            );
+            return updatedReader;
+        } catch (err) {
+            console.log(err);
+            return err;
+        }
+        },
+    },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'reader',
+    }
+)

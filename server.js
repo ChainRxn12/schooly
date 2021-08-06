26524d805ecccf6bd5e18124587d0e4aba27b340
Billy Dorman <b.d.dorman12@gmail.com>
2021-08-06 13:19:15 -0500

    debugged some routes, edited css, views files

--- server.js
+++ server.js
@@ -6,12 +6,10 @@ const helpers = require("./utils/helpers");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
 const exphbs = require("express-handlebars");
-const hbs = exphbs.create("view engine", "handlebars");
+// Set up Handlebars.js engine with custom helpers
+const hbs = exphbs.create({ helpers });
+//const hbs = exphbs.create("view engine", "handlebars");
 const session = require("express-session");
 const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
@@ -30,8 +28,17 @@ const sess = {
 
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log("Now Listening"));
+  app.listen(PORT, () => console.log("Now Listening localhost:3001"));
 });

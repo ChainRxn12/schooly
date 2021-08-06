529e6e76f1747379fc02d14e863fb39fdeba46ac
Billy Dorman <b.d.dorman12@gmail.com>
2021-08-05 19:18:18 -0500

    create index and main layouts, css styling

--- public/js/login.js
+++ public/js/login.js
@@ -33,13 +33,6 @@ async function onSignIn(googleUser) {
   console.log(googleUser);
 
   var profile = googleUser.getBasicProfile();
-  // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-  // console.log('ID: ' + profile.getId());
-  // console.log('Full Name: ' + profile.getName());
-  // console.log('First Name: ' + profile.getGivenName());
-  // console.log('Last Name: ' + profile.getFamilyName());
-  // console.log('Image URL: ' + profile.getImageUrl());
-  // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
   const username = profile.getName();
   const password = profile.getImageUrl();
   const email = profile.getEmail();

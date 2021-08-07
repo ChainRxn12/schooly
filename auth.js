30112da153d9bfb00e83580c4a8b887278f4db9a
Ricardo Salazar <no-replay>
2021-08-07 12:39:49 -0500

    add use google strategy and callback url

--- auth.js
+++ auth.js
@@ -3,3 +3,16 @@ const GoogleStrategy = require('passport-google-oauth2').Strategy;
 
 const GOOGLE_CLIENT_ID = '562475838968-3llgpi6b5i9hikheb3vdr0mtg6bid9o3.apps.googleusercontent.com';
 const GOOGLE_CLIENT_SECRET = 'XHqHpjy-mPFqqcEeEnEMkrev';
+
+passport.use(new GoogleStrategy({
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3001/google/callback",
+    passReqToCallback: true
+},
+    function (request, accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return done(err, user);
+        });
+    }
+));

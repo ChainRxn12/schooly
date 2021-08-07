74ecf978e85206d36488c4f55b62c142c482222d
Ricardo Salazar <no-replay>
2021-08-07 12:06:19 -0500

    add google credentials

--- auth.js
+++ auth.js
@@ -1,2 +1,5 @@
 const passport = require("passport");
 const GoogleStrategy = require('passport-google-oauth2').Strategy;
+
+const GOOGLE_CLIENT_ID = '562475838968-3llgpi6b5i9hikheb3vdr0mtg6bid9o3.apps.googleusercontent.com';
+const GOOGLE_CLIENT_SECRET = 'XHqHpjy-mPFqqcEeEnEMkrev';

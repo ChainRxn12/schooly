fff71aeb33d4a0a700c4c898126c57c666f49c0a
Ricardo Salazar <no-replay>
2021-08-07 12:02:46 -0500

    add callback, protected login, logout and authenticate

--- server.js
+++ server.js
@@ -12,3 +12,34 @@ const PORT = process.env.PORT || 3001;
 app.use(session({ secret: "Schooly" }));
 app.use(passport.initialize());
 app.use(passport.session());
+
+app.get("/", (req, res) => {
+    res.send('<a href="/auth/google">Authenticate with Google</a>')
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope: ['email', 'profile' ] })
+)
+
+app.get('/google/callback',
+  passport.authenticate('google', {
+      successRedirect: '/protected',
+      failureRedirect: '/auth/failure',
+  })
+);
+
+app.get('/auth/failure', (req, res) =>{
+    res.send('Something went wrong!')
+});
+
+app.get('/protected', isLoggedIn, (req, res) => {
+    res.send(`Welcome! ${req.user.displayName}`);
+});
+
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.session.destroy();
+    res.send('Goodbye!');
+});
+
+app.listen(PORT, () => console.log('Now Listening'));

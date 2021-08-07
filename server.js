f6a21306f981a4cad0515dfacbee0c50b909aca7
Ricardo Salazar <no-replay>
2021-08-07 11:57:22 -0500

    add function to login

--- server.js
+++ server.js
@@ -2,3 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const passport = require('passport');
 require('./auth');
+
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}

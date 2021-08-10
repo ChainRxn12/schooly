c876ba52394d2288f6a2938a9f7e2389ae8bcdcc
jafet <jafet.manauth@Hotmail.com>
2021-08-10 18:43:57 -0500

    pushing to sync with everyone else

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -14,7 +14,7 @@ router.get('/', async (req, res) => {
 
     const users = userData.map((project) => project.get({ plain: true }));
 //console.log("-----------------------------" + users + "--------------------------")
-    res.render('homepage', {
+    res.render('index', {
       users,
       // Pass the logged in flag to the template
       logged_in: req.session.logged_in,
@@ -26,7 +26,7 @@ router.get('/', async (req, res) => {
 
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/dashboard');
     return;
   }
 
@@ -36,11 +36,12 @@ router.get('/login', (req, res) => {
 
 router.get('/signup', (req, res) => {
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/dashboard');
     return;
   }
 
   res.render('signup')
 });
 
-  module.exports = router;
+
+module.exports = router;

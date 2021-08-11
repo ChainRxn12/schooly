c3ca07fe11b263db9a21de4ca4d4528feb7a0c94
Billy Dorman <b.d.dorman12@gmail.com>
2021-08-10 23:56:21 -0500

    created roster handlebars, populates students

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -25,28 +25,45 @@
 //     }
 //     rosterEl.innerHTML = rosterList
 // }
-let studentData = [];
+// let studentData = [];
 
-async function newFormHandler(event) {
+// async function newFormHandler(event) {
+//     event.preventDefault();
+
+//     var studentRoster = await fetch (`/api/students`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(studentRoster),
+//     })
+//     .then(function (response) {
+//         return response.json();
+//     })
+//     .then(function (data) {
+//         studentData = data;
+//         console.log(data[0].first_name);
+//         console.log(data);
+//     })
+// }
+
+// document
+// .querySelector('.roster-btn')
+// .addEventListener('click', newFormHandler)
+
+const renderRoster = async (event) => {
     event.preventDefault();
-
-    var studentRoster = await fetch (`/api/students`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(studentRoster),
-    })
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        studentData = data;
-        console.log(data[0].first_name);
-        console.log(data);
-    })
-}
-
-document
-.querySelector('.roster-btn')
-.addEventListener('click', newFormHandler)
+    const id = document.querySelector('#{{student.id}}').value.trim();
+    
+    const response = await fetch(`/api/students/${id}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            });
+            if (response.ok) {
+                        document.location.replace('/dashboard/teacher/roster');
+                    } else {
+                        alert('Unable to Register Teacher.');
+                    };
+};

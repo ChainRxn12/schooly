b85ac9f61a7c3db28f9405d584cf63edec220e01
Billy Dorman <b.d.dorman12@gmail.com>
2021-08-10 22:47:54 -0500

    working on teacher/ student roster

--- public/js/teacher.js
+++ public/js/teacher.js
@@ -1,27 +1,52 @@
-const rosterBtn = document.getElementById('roster-btn');
-let rosterArray = [
-    'Billy Dorman',
-    'Jafet Manauth',
-    'Zion Flores',
-    'Ricardo Salazar'
-];
-const rosterEl = document.getElementById('roster-el');
+// const rosterBtn = document.getElementById('roster-btn');
+// let rosterArray = [
+//     'Billy Dorman',
+//     'Jafet Manauth',
+//     'Zion Flores',
+//     'Ricardo Salazar'
+// ];
+//  const rosterEl = document.getElementById('roster-el');
 
-rosterBtn.addEventListener('click', function() {
-   renderRoster();
-})
+// rosterBtn.addEventListener('click', function() {
+//    renderRoster();
+// })
 
-const renderRoster = () => {
-    let rosterList = "";
-    for(let i = 0; i< rosterArray.length; i++) {
-        rosterList += `
-        <li>
-            <a href='#'>
-                ${rosterArray[i]}
-            </a>
-        </li>
-        `;
-        console.log(rosterList)
-    }
-    rosterEl.innerHTML = rosterList
+// const renderRoster = () => {
+//     let rosterList = "";
+//     for(let i = 0; i< rosterArray.length; i++) {
+//         rosterList += `
+//         <li>
+//             <a href='#'>
+//                 ${rosterArray[i]}
+//             </a>
+//         </li>
+//         `;
+//         console.log(rosterList)
+//     }
+//     rosterEl.innerHTML = rosterList
+// }
+let studentData = [];
+
+async function newFormHandler(event) {
+    event.preventDefault();
+
+    var studentRoster = await fetch (`/api/students`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(studentRoster),
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        studentData = data;
+        console.log(data[0].first_name);
+        console.log(data);
+    })
 }
+
+document
+.querySelector('.roster-btn')
+.addEventListener('click', newFormHandler)
